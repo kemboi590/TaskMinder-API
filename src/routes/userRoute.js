74541ca8c81d1bef33dc29de81c9b6a6ae6e265d
kemboi590 <bkemboi590@gmail.com>
2023-07-25 19:41:34 +0300

    user details

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,14 +2,12 @@ import {
   registerUser,
   loginUser,
   getAllUsers,
-  updateProfileURL
 } from "../controllers/userController.js";
 
 const user = (app) => {
   app.route("/auth/register").post(registerUser);
   app.route("/auth/login").post(loginUser);
   app.route("/users").get(getAllUsers);
-  app.route("/picture").put(updateProfileURL)
 };
 
 export default user;
